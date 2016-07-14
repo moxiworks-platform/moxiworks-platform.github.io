91f9475ae2d1a22596c6ae725d0653fa23a9f835
Tres Wong-Godfrey <tres@clean.io>
2016-07-14 14:55:27 -0700

    doc updates

--- publish.sh
+++ publish.sh
@@ -4,6 +4,9 @@ git commit -a -m "doc updates"
 git fetch origin
 git merge origin/develop
 rake publish
+git checkout gh-pages
+git fetch origin
+git merge origin/gh-pages
 git checkout master
 git merge gh-pages
 git checkout develop

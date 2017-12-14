b98b84219069b851e6911e50e1405c0b7b99cf6a
Tres Wong-Godfrey <tres@clean.io>
2017-12-14 13:20:39 -0800

    doc updates

--- publish.sh
+++ publish.sh
@@ -3,6 +3,8 @@
 git commit -a -m "doc updates"
 git fetch origin
 git merge origin/develop
+git checkout master
+git merge develop 
 bundle exec middleman build --clean
 git checkout gh-pages
 git fetch origin
@@ -11,9 +13,7 @@ mkdir old
 mv * old
 mv old/build/* ./
 rm -r old
-git commit -a -m "doc updates"
-git checkout master
-git merge gh-pages
+git commit -a -m "doc build"
 git checkout develop
 git push origin master
 git push origin develop

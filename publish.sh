ddb25181984ae5da9ccc3cf1d88f790903beb8fe
Tres Wong-Godfrey <tres@clean.io>
2017-12-14 13:27:42 -0800

    update publish script
           o        o----o   ____o    e----o   o   o  o--.
          /        /        /        /        /|  /  /
         /        o---o    /   o-o  o---o    / | /  /
        /        /        |     /  /        /  |/  /
       o-----o  o-----o   o____o  o----o   o   o

--- publish.sh
+++ publish.sh
@@ -7,7 +7,6 @@ git checkout master
 git merge develop 
 bundle exec middleman build --clean
 git checkout gh-pages
-git fetch origin
 git merge origin/gh-pages
 mkdir old
 mv * old
@@ -15,9 +14,9 @@ mv old/build/* ./
 rm -r old
 git commit -a -m "doc build"
 git checkout develop
-git push origin master
-git push origin develop
-git push origin gh-pages
-git push upstream master
-git push upstream develop
-git push upstream gh-pages
+git push origin master --force
+git push origin develop --force
+git push origin gh-pages --force
+git push upstream master --force
+git push upstream develop --force
+git push upstream gh-pages --force

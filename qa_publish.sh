b3cf7b0bcd4189d7e82220954fbd2825308821b5
Tres Wong-Godfrey <tres@wong-godfrey.com>
2020-03-18 12:11:35 -0700

    Add qa_publish.sh Script
      |\___
    (:o ___(
      |/      Ghost

---
+++ qa_publish.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git fetch qa
+git merge qa/develop
+bundle exec middleman build --clean
+git checkout gh-pages
+git merge qa/gh-pages
+mkdir old
+mv * old
+mv old/build/* ./
+rm -r old
+git add *
+git commit -a -m "doc build"
+git checkout master
+git merge gh-pages 
+git checkout develop
+git push qa master
+git push qa develop
+git push qa gh-pages
